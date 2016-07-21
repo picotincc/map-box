4eb402b3de8811773305c6b0d67db21771db58c9
[maybeShuo] <[546537825@qq.com]>
2016-07-21 13:40:56 +0800

    Add ex.

--- src/mb/map/MapView.js
+++ src/mb/map/MapView.js
@@ -1,6 +1,8 @@
 import AdaptiveMapView from "sap/a/map/MapView";
 import TileLayer from "sap/a/map/layer/TileLayer";
 
+import ExampleLayer from "./layer/ExampleLayer";
+
 export default class MapView extends AdaptiveMapView
 {
     afterInit()
@@ -15,5 +17,13 @@ export default class MapView extends AdaptiveMapView
             url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
         });
         this.addLayer(this.tileLayer);
+
+        this.exampleLayer = new ExampleLayer({
+            startLocation: [ 31.9790247, 118.7548084 ],
+            endLocation: [ 32.04389, 118.77881 ]
+        });
+        this.addLayer(this.exampleLayer);
+        this.exampleLayer.drawRoute();
+        this.exampleLayer.fitBounds();
     }
 }
